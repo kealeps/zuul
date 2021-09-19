9c19bb8d3f6ed9b77f9d56e3ab7636b6b3f1a835
Kevin Perez <kevinp@cronapis.com>
2021-09-19 14:23:54 -0400

    first commit

---
+++ src/main/java/com/zuul/app/zuul/filters/PreTimeFilter.java
@@ -0,0 +1,48 @@
+package com.zuul.app.zuul.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PreTimeFilter extends ZuulFilter{
+
+    private static Logger log = LoggerFactory.getLogger(PreTimeFilter.class);
+
+    @Override
+    public Object run() throws ZuulException {
+
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		
+		log.info(String.format("%s request enrutado a %s", request.getMethod(), request.getRequestURL().toString()));
+		
+		Long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio", tiempoInicio);
+		
+		return null;
+	}
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+    
+}
